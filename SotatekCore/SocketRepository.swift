db7560be8779a63dda4317debc9618cee764a399
Thanh Tran <thanh.tran@sotatek.com>
2016-10-17 14:23:17 +0700

    Socket repository

--- SotatekCore/SocketRepository.swift
+++ SotatekCore/SocketRepository.swift
@@ -14,24 +14,42 @@ class SocketRepository {
     let notifier = Notifier.socketNoitfier
     
     var socketRequest = SocketRequest()
-    var dtoTypes = [BaseEntity.entityName: BaseDto.self]
+    var dtoTypes = [BaseDto.entityName: BaseDto.self]
+    var entityTypes = [BaseEntity.entityName: BaseEntity.self]
     
     private init() {
+    }
+    
+    open func connect() {
+        socketRequest.connect()
+    }
+    
+    open func disconnect() {
+        socketRequest.disconnect()
     }
     
     open func addDataType(_ type: BaseDto.Type) {
         dtoTypes[type.self.entityName] = type
     }
     
+    open func addDataType(_ type: BaseEntity.Type) {
+        entityTypes[type.self.entityName] = type
+    }
+    
     open func onReceiveData(json: JSON) {
         for (key,subJson): (String, JSON) in json {
-            if let baseCache = CacheFactory.getCache(forEntity: key) {
-                let entity = baseCache.save(subJson) as! Serializable
-                notifier.notifyObservers(Constant.COMMAND_RECEIVE_DATA, data: SocketData(name: key, data: entity))
-            }
-            else if let dtoType = dtoTypes[key] {
-                let dto = dtoType.init(fromJson: json)
+//            if let baseCache = CacheFactory.getCache(forEntity: key) {
+//                let entity = baseCache.save(subJson) as! Serializable
+//                notifier.notifyObservers(Constant.COMMAND_RECEIVE_DATA, data: SocketData(name: key, data: entity))
+//            }
+//            else
+            if let dtoType = dtoTypes[key] {
+                let dto = dtoType.init(fromJson: subJson)
                 notifier.notifyObservers(Constant.COMMAND_RECEIVE_DATA, data: SocketData(name: key, data: dto))
+            }
+            else if let entityType = entityTypes[key] {
+                let entity = entityType.init(fromJson: subJson)
+                notifier.notifyObservers(Constant.COMMAND_RECEIVE_DATA, data: SocketData(name: key, data: entity))
             }
             else {
                 print("Unknown data type: \(key)")
--- SotatekCore/SocketRequest.swift
+++ SotatekCore/SocketRequest.swift
@@ -9,4 +9,27 @@
 import Foundation
 
 public class SocketRequest {
+    let notifier = Notifier.socketNoitfier
+    
+    open func connect() {
+        startMock()
+    }
+    
+    open func disconnect() {
+        
+    }
+    
+    func startMock() {
+        let startTime = Util.currentTime()
+        _ = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(notify), userInfo: nil, repeats: true)
+    }
+    
+    @objc func notify() {
+        let id = Util.currentTime()
+        let time = Util.currentTime()
+        let chatLine = ChatLineEntity(id: id, mediaId: 0, userId: 0, type: 0, content: "Message \(id)", createdAt: time, updatedAt: time)
+        let socketData = SocketData(name: "chatLine", data: chatLine)
+        notifier.notifyObservers(Constant.COMMAND_RECEIVE_DATA, data: socketData)
+        
+    }
 }
